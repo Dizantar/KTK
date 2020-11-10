78e722509a5f20191b614a710037b44c9ac7dcd3
Diz <diz@diz.com>
2020-11-09 22:29:55 -0500

    Fix home path

--- scripts/ktk-tools.sh
+++ scripts/ktk-tools.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 if [[ "$1" == "" ]]
 then
-  hostname -I && python3 -m http.server --directory /KTK/tools 8888
+  hostname -I && python3 -m http.server --directory ~/KTK/tools 8888
 else
-  hostname -I && python3 -m http.server --directory /KTK/tools $1
+  hostname -I && python3 -m http.server --directory ~/KTK/tools $1
 fi
